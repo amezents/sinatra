b457581139dde68db99bba7437881d81a20cbf91
Mezentsev Arkady <amezents@gmail.com>
2017-12-08 20:43:07 +0300

    Add static html pages and dynamic ones

--- app.rb
+++ app.rb
@@ -17,10 +17,38 @@ get  "/cat"  do
   "Meow Meow I AM CAT"
 end
 
+get "/greet_to_parts/:first/:last" do
+    "Hello there #{params[:first]} #{params[:last]}!"
+end
+
+get "/greet/:person" do
+    "Hello there #{params[:person]}!\n params[:person]`s type is : #{params[:person].class}\n :person`s type is #{:person.class}  "
+end
+
+get  "/square/:number"  do
+  if /\d*\..*\d/ =~ params[:number] then  res = params[:number].to_f * params[:number].to_f
+  elsif   /\d+/ =~ params[:number] then  res = params[:number].to_i * params[:number].to_i
+  else res = "Not a digit"
+  end
+  "#{res}"
+end
+
+get  "/sinatra_st"  do
+  "<h3>I love <a href=http://www.sinatrarb.com/>Sinatra</a>!</h3>"
+end
+
+get "/not_my_cat" do
+   send_file "cat.html"
+end
+
+get  "/Sinatra_st"  do
+   send_file "sinatra.html"
+end
 
 
-
-
+get  "/sinatra"  do
+   erb :sinatra
+end
 
 
 
